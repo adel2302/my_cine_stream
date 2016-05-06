1097ea598473619621f48e1d631e8f8922521ae2
Adel Chouanine <a.chouanine@hotmail.fr>
2016-05-07 00:43:44 +0200

    Add Movies MVC, gem bootstrap and gem simple_form

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,57 @@
+class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update]
+
+  def index
+  	@movies = Movie.all
+  end
+
+  def new
+  	@movie = Movie.new
+  end
+
+  def edit
+  end
+
+  def show
+  	@movie = Movie.find(params[:id])
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    respond_to do |format|
+      if @movie.save
+        format.html { redirect_to @movie, notice: 'movie was successfully created.' }
+        format.json { render :show, status: :created, location: @movie }
+      else
+        format.html { render :new }
+        format.json { render json: @movie.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /movies/1
+  # PATCH/PUT /movies/1.json
+  def update
+    respond_to do |format|
+      if @movie.update(movie_params)
+        format.html { redirect_to @movie, notice: 'movie was successfully updated.' }
+        format.json { render :show, status: :ok, location: @movie }
+      else
+        format.html { render :edit }
+        format.json { render json: @movie.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def movie_params
+      params.require(:movie).permit(:title, :path)
+    end
+end
